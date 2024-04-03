df8ed43f4dbba79dc5c1be5fcc225a7dbb983497
patrick espinoza <patrickespinoza38@gmail.com>
2024-04-03 13:36:55 -0600

    indica monto evisto y dia

--- practica-1.js
+++ practica-1.js
@@ -1,13 +1,14 @@
 // monto prevsto para ese mismo dia 
-const montoPrevisto = 11;
+const montoPrevisto = 5;
 
 // crear una variable para guardar el numero de ordenes de un dia 
 const ordenesEfectivas = 11;
 
 // crea los dias del mes para saber si es impar o no 
-const diasDelMes = 30
+const diaDelMes = 29
 
 function ProyectarCapacidad() {
+
 
     if (ordenesEfectivas > montoPrevisto) {
         console.log("Carlos, debes incrementar la capacidad en el siguiente dia")
@@ -15,7 +16,23 @@ function ProyectarCapacidad() {
         console.log("Carlos, debes reducir la capacidad en el siguiente dia")
     } else {
         console.log ("Carlos, vas bien sigue asi")
+    } 
+    
+} 
+
+function diasImpares(){
+
+   const diaImpar = diaDelMes % 2;
+
+   if (diaImpar === 0) {
+        console.log("El dia es par!!");
+    } else {
+        console.log("El dia es impar!!")
     }
+
+    
 }
 
+
 ProyectarCapacidad()
+diasImpares()
